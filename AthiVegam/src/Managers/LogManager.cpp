6c6d6475ccb82be9537b566fbf1df249589a3a4c
Srinath Upadhyayula <srinathupadhyayula@gmail.com>
2022-01-24 14:24:28 +0530

    Updated .cpp files to use correct include path

--- AthiVegam/src/Managers/LogManager.cpp
+++ AthiVegam/src/Managers/LogManager.cpp
@@ -1,5 +1,5 @@
-#include "Managers/LogManager.h"
-#include "Log.h"
+#include "AthiVegam/Managers/LogManager.h"
+#include "AthiVegam/Log.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 #include <memory>
@@ -12,7 +12,7 @@ namespace AthiVegam::Managers
 		consoleSink->set_pattern("%^[%Y-%m-%d %H:%M:%S.%e] %v%$");
 
 		std::vector<spdlog::sink_ptr> sinks{ consoleSink };
-		auto logger = std::make_shared<spdlog::logger>(ATHI_VEGAM_DEFAULT_LOGGER_NAME, sinks.begin(), sinks.end());
+		auto logger = std::make_shared<spdlog::logger>(AV_DEFAULT_LOGGER_NAME, sinks.begin(), sinks.end());
 		logger->set_level(spdlog::level::trace);
 		logger->flush_on(spdlog::level::trace);
 		spdlog::register_logger(logger);
